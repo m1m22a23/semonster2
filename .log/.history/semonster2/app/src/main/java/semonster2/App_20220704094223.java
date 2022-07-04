aec409590da807d5bebeff7fee7c430733c04b26
m1m22a23 <m1m22a23@st.oit.ac.jp>
2022-07-04 09:59:31 +0900

    change greeting message

--- .log/.history/semonster2/app/src/main/java/semonster2/App_20220704094223.java
+++ .log/.history/semonster2/app/src/main/java/semonster2/App_20220704094223.java
@@ -5,7 +5,7 @@ package semonster2;
 
 public class App {
     public String getGreeting() {
-        return "こんにちは SEMonster";
+        return "Hello World!";
     }
 
     public static void main(String[] args) {
